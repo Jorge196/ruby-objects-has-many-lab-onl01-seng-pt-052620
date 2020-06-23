ddbd2f03e0efdecaea51c63169bd09fff18e943b
Jorge Ramirez <joramirez96@gmail.com>
2020-06-23 21:30:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,11 +4,15 @@ class Song
     @name = name
   end
 
+  def self.all
+    @all
+  end
+  
   def artist_name
     if @artist == nil
       return nil
     else
       @artist.name
     end
-  end 
+  end
 end
