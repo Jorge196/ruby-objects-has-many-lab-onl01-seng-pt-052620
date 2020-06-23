980e795ced50e5bb99a1f801ff1a065b682c6033
Jorge Ramirez <joramirez96@gmail.com>
2020-06-23 21:31:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,12 @@
 class Song
   attr_accessor :artists, :name
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
 
-  def self.all
-    @all
-  end
-  
+
   def artist_name
     if @artist == nil
       return nil
